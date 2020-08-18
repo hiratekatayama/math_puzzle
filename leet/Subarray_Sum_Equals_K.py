4f1c8bb6b692e20a235c0cd9688806caa404e21e
hiratekatayama <zacstarl@yahoo.co.jp>
2020-08-18 09:01:36 +0900

    add algorithm

--- leet/Subarray_Sum_Equals_K.py
+++ leet/Subarray_Sum_Equals_K.py
@@ -14,10 +14,10 @@ class Solution(object):
         count = 0
         sum = [0] * (len(nums) + 1)
         sum[0] = 0
-        for i in range(1, len(nums)):
+        for i in range(1, len(nums) + 1):
             sum[i] = sum[i - 1] + nums[i - 1]
         for start in range(len(nums)):
-            for end in range(start+1, len(nums)):
+            for end in range(start+1, len(nums) + 1):
                 if sum[end] - sum[start] == k:
                     count += 1
         return count
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     test = Solution()
 
     nums = [1,1,1]
-    k    = 2
+    k    = 3
     check = test.subarraySum(nums, k)
     print(check)
 
